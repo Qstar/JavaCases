59176177e26eb1a719ee38a76e02d1abd26efb5b
qstar <davesla2012@hotmail.com>
2016-09-05 14:26:18 +0800

    Swing

--- src/main/java/Swing/SwingsDemo.java
+++ src/main/java/Swing/SwingsDemo.java
@@ -10,18 +10,18 @@ public class SwingsDemo extends JFrame {
         contentPane.setLayout(new FlowLayout());
 
         Box myHorizontalBox = Box.createHorizontalBox();
-        Box myVerticleBox = Box.createVerticalBox();
+        Box myVerticalBox = Box.createVerticalBox();
 
         myHorizontalBox.add(new JButton("My Button 1"));
         myHorizontalBox.add(new JButton("My Button 2"));
         myHorizontalBox.add(new JButton("My Button 3"));
 
-        myVerticleBox.add(new JButton(new ImageIcon(path + "//Image1.jpg")));
-        myVerticleBox.add(new JButton(new ImageIcon(path + "//Image2.jpg")));
-        myVerticleBox.add(new JButton(new ImageIcon(path + "//Image3.jpg")));
+        myVerticalBox.add(new JButton(new ImageIcon(path + "//Image1.jpg")));
+        myVerticalBox.add(new JButton(new ImageIcon(path + "//Image2.jpg")));
+        myVerticalBox.add(new JButton(new ImageIcon(path + "//Image3.jpg")));
 
         contentPane.add(myHorizontalBox);
-        contentPane.add(myVerticleBox);
+        contentPane.add(myVerticalBox);
 
         pack();
         setVisible(true);
