1e293578663593c6687c74cbfa7cee53150c9571
shuaiqiao <shuaiqiao@sohu-inc.com>
2018-04-24 16:16:02 +0800

    Composite pattern

---
+++ src/main/java/designPattern/Composite/TextFile.java
@@ -0,0 +1,14 @@
+package designPattern.Composite;
+
+public class TextFile extends File{
+
+    public TextFile(String name) {
+        super(name);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("这是文本文件，文件名：" + super.getName());
+    }
+
+}
