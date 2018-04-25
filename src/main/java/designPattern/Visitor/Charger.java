6ddb1e666ca978d3e69b0691bdaa17808597b3a3
shuaiqiao <shuaiqiao@sohu-inc.com>
2018-04-25 15:16:48 +0800

    Visitor Pattern

---
+++ src/main/java/designPattern/Visitor/Charger.java
@@ -0,0 +1,16 @@
+package designPattern.Visitor;
+
+// 具体访问者：划价员
+public class Charger extends Visitor {
+
+    @Override
+    public void visitor(MedicineA a) {
+        System.out.println("划价员：" + name + "给药" + a.getName() + "划价:" + a.getPrice());
+    }
+
+    @Override
+    public void visitor(MedicineB b) {
+        System.out.println("划价员：" + name + "给药" + b.getName() + "划价:" + b.getPrice());
+    }
+
+}
