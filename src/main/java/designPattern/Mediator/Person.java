836e766d519f199d62dff0d70dce1817625d25ac
shuaiqiao <shuaiqiao@sohu-inc.com>
2018-04-25 12:05:18 +0800

    mediator pattern

---
+++ src/main/java/designPattern/Mediator/Person.java
@@ -0,0 +1,12 @@
+package designPattern.Mediator;
+
+public abstract class Person {
+    protected String name;
+    protected Mediator mediator;
+
+    Person(String name, Mediator mediator) {
+        this.name = name;
+        this.mediator = mediator;
+    }
+
+}
